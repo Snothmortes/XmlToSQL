ce65ab7c69ffd621b0a64f4832e5d53b210a7990
Glen Jakobsen <glen.jakobsen@gmail.com>
2021-06-24 14:18:31 +0200

    changes

--- Program.cs
+++ Program.cs
@@ -12,9 +12,9 @@
     class Program
     {
         static void Main() {
-            using (var con = new SqlConnection(@"server=(localdb)\MyInstance;database=CalibTable;integrated security=true")) {
+            using (var con = new SqlConnection(@"server=(localdb)\MyInstance;database=CalibStore;integrated security=true")) {
                 var ds = new DataSet();
-                ds.ReadXml("C:/Users/Glen/Desktop/A001.calib.xml");
+                ds.ReadXml("C:/Users/GLJ/Desktop/A001.calib.xml");
 
                 var dtProj = ds.Tables["Project"];
                 var dtLogger = ds.Tables["Logger"];
@@ -22,30 +22,6 @@
                 var dtCalib = ds.Tables["Calibration"];
 
                 con.Open();
-                using (var bc = new SqlBulkCopy(con)) {
-                    bc.DestinationTableName = "Projects";
-                    bc.ColumnMappings.Add("No", "No");
-                    bc.ColumnMappings.Add("Ini", "Ini");
-                    bc.ColumnMappings.Add("CalibrationType", "CalibrationType");
-                    bc.WriteToServer(dtProj);
-                }
-
-                using (var bc = new SqlBulkCopy(con)) {
-                    bc.DestinationTableName = "Loggers";
-                    bc.ColumnMappings.Add("name", "Name");
-                    bc.ColumnMappings.Add("Serial", "Serial");
-                    bc.ColumnMappings.Add("idx", "Idx");
-                    bc.WriteToServer(dtLogger);
-                }
-
-                using (var bc = new SqlBulkCopy(con)) {
-                    bc.DestinationTableName = "Transducers";
-                    bc.ColumnMappings.Add("type", "Type");
-                    bc.ColumnMappings.Add("serial", "Serial");
-                    bc.ColumnMappings.Add("nomlow", "NomLow");
-                    bc.ColumnMappings.Add("nomhigh", "NomHigh");
-                    bc.WriteToServer(dtTrasducer);
-                }
 
                 using (var bc = new SqlBulkCopy(con)) {
                     bc.DestinationTableName = "Calibrations";
@@ -58,6 +34,28 @@
                     bc.ColumnMappings.Add("minv", "MinV");
                     bc.ColumnMappings.Add("maxv", "MaxV");
                     bc.WriteToServer(dtCalib);
+                }
+                using (var bc = new SqlBulkCopy(con)) {
+                    bc.DestinationTableName = "Loggers";
+                    bc.ColumnMappings.Add("name", "Name");
+                    bc.ColumnMappings.Add("Serial", "Serial");
+                    bc.ColumnMappings.Add("idx", "Idx");
+                    bc.WriteToServer(dtLogger);
+                }
+                using (var bc = new SqlBulkCopy(con)) {
+                    bc.DestinationTableName = "Projects";
+                    bc.ColumnMappings.Add("No", "No");
+                    bc.ColumnMappings.Add("Ini", "Ini");
+                    bc.ColumnMappings.Add("CalibrationType", "CalibrationType");
+                    bc.WriteToServer(dtProj);
+                }
+                using (var bc = new SqlBulkCopy(con)) {
+                    bc.DestinationTableName = "Transducers";
+                    bc.ColumnMappings.Add("type", "Type");
+                    bc.ColumnMappings.Add("serial", "Serial");
+                    bc.ColumnMappings.Add("nomlow", "NomLow");
+                    bc.ColumnMappings.Add("nomhigh", "NomHigh");
+                    bc.WriteToServer(dtTrasducer);
                 }
             }
         }
